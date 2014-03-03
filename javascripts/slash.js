f6a8bbc866965a470a007804200e4fbd5c15d11b
hemanth.hm <hemanth.hm@gmail.com>
2014-03-03 16:22:49 +0530

    Site updated at 2014-03-03 10:52:49 UTC

--- javascripts/slash.js
+++ javascripts/slash.js
@@ -1,37 +1,4 @@
 (function($){
-	// Open external links in new window
-	var externalLinks = function(){
-		var host = location.host;
-
-		$('body').on('click', 'a', function(e){
-			var href = this.href,
-				link = href.replace(/https?:\/\/([^\/]+)(.*)/, '$1');
-
-			if (link != '' && link != host && !$(this).hasClass('fancybox')){
-				window.open(href);
-				e.preventDefault();
-			}
-		});
-	};
-
-	// Append caption after pictures
-	var appendCaption = function(){
-		$('.entry-content').each(function(i){
-			var _i = i;
-			$(this).find('img').each(function(){
-				var alt = this.alt;
-
-				if (alt != ''){
-					$(this).after('<span class="caption">'+alt+'</span>');
-				}
-
-				$(this).wrap('<a href="'+this.src+'" title="'+alt+'" class="fancybox" rel="gallery'+_i+'" />');
-			});
-		});
-	};
-
-	externalLinks(); // Delete or comment this line to disable opening external links in new window
-	appendCaption(); // Delete or comment this line to disable caption
 
 	var mobilenav = $('#mobile-nav');
 
@@ -43,16 +10,14 @@
 
 	mobilenav.on('click', '.menu .button', function(){
 		if (!$(this).hasClass('on')){
-			var width = $(this).width() + 42;
-			$(this).addClass('on').next().show().css({width: width});
+			$(this).addClass('on').next().show();
 		} else {
 			$(this).removeClass('on').next().hide();
 		}
 	}).on('click', '.search .button', function(){
 		if (!$(this).hasClass('on')){
-			var width = mobilenav.width() - 51;
 			mobilenav.children('.menu').children().eq(0).removeClass('on').next().hide();
-			$(this).addClass('on').next().show().css({width: width}).children().children().eq(0).focus();
+			$(this).addClass('on').next().show().children().children().eq(0).focus();
 		} else {
 			$(this).removeClass('on').next().hide().children().children().eq(0).val('');
 		}
